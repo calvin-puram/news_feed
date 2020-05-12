d982d0169dacdae72778f2feaa0e09e23aaa433f
calvin <puram.calvin@gmail.com>
2020-05-12 10:56:43 +0100

    save users data to cookies

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,14 @@
+import Cookie from 'js-cookie';
+
 export default function ({ idToken, expiresIn }, { email, avatar }) {
   const tokenExpiration = Date.now() + expiresIn * 1000;
   localStorage.setItem('jwt', idToken);
   localStorage.setItem('expiresIn', tokenExpiration);
   localStorage.setItem('email', email);
   localStorage.setItem('avatar', avatar);
+  Cookie.set('jwt', idToken);
+  Cookie.set('expiresIn', tokenExpiration);
+  Cookie.set('email', email);
+  Cookie.set('avatar', avatar);
+  console.log(Cookie.get());
 }
